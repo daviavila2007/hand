4569f40ec509b454f8c42494102cfb988b5a9bc6
daviavila2007 <139869720+daviavila2007@users.noreply.github.com>
2025-05-04 13:47:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function sobre(){
+    window.location.href = "sobre.html"
+}
+function regras(){
+    window.location.href = "regras.html"
+}
+function voltar(){
+    window.location.href = "index.html"
+}
+function voltar2(){
+    window.location.href = "sobre.html"
+}
